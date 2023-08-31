67eaacf3ec0412e339e2ea32e1da27a502140410
Christos Ploutarchou <cploutarchou@gmail.com>
2023-08-31 21:38:47 +0300

    start implementing bybit interface

--- bybit/bybit.go
+++ bybit/bybit.go
@@ -1,1 +1,31 @@
 package bybit
+
+import (
+	"github.com/cploutarchou/crypto-sdk-suite/bybit/client"
+	"github.com/cploutarchou/crypto-sdk-suite/bybit/market"
+)
+
+type Bybit interface {
+	Market() market.Market
+}
+
+type bybitImpl struct {
+	market    market.Market
+	client    *client.Client
+	isTestNet bool
+}
+
+func (b bybitImpl) Market() market.Market {
+	return b.market
+}
+
+func NewBybit(key, secretKey string, isTestNet bool) Bybit {
+	c := client.NewClient(key, secretKey)
+	c.IsTestNet = isTestNet
+	return &bybitImpl{
+		isTestNet: isTestNet,
+		client:    c,
+		market:    market.NewMarket(c),
+	}
+
+}

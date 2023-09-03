acdd771af4047ed17f3b6ac31ef1297694ab993d
Christos Ploutarchou <cploutarchou@gmail.com>
2023-09-03 23:11:20 +0300

    extend ws functionality

--- bybit/ws/client/ws.go
+++ bybit/ws/client/ws.go
@@ -15,20 +15,6 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// Constants grouped for better clarity
-const (
-	DefaultReqID        = "100001"
-	PingOperation       = "ping"
-	AuthOperation       = "auth"
-	LocalNetEnvironment = "localhost:8080"
-	DefaultScheme       = "wss"
-	LocalhostScheme     = "ws"
-	ApiV5               = "v5"
-	PingInterval        = 20 * time.Second
-	ReconnectionRetries = 3
-	ReconnectionDelay   = 2 * time.Second
-)
-
 // WSPingMsg represents the structure for WebSocket ping messages
 type WSPingMsg struct {
 	Op    string `json:"op"`
@@ -50,7 +36,7 @@ const (
 )
 
 type WSClient struct {
-	conn      *websocket.Conn
+	Conn      *websocket.Conn
 	mu        sync.Mutex
 	readMu    sync.Mutex
 	closeOnce sync.Once
@@ -63,6 +49,8 @@ type WSClient struct {
 	isTest    bool
 	channel   ChannelType
 }
+
+type ChannelType string
 
 // NewWSClient initializes a new WSClient
 func NewWSClient(apiKey, apiSecret string, isTestNet, isPublic bool) (*WSClient, error) {
@@ -105,7 +93,7 @@ func (c *WSClient) connect() error {
 		return fmt.Errorf("failed to dial %s: %v", url, err)
 	}
 
-	c.conn = conn
+	c.Conn = conn
 	c.logger.Printf("Connected to %s", url)
 	return nil
 }
@@ -162,14 +150,14 @@ func (c *WSClient) sendPingAndHandleReconnection() {
 		return
 	}
 
-	if err := c.conn.WriteMessage(websocket.TextMessage, jsonData); err != nil {
+	if err := c.Conn.WriteMessage(websocket.TextMessage, jsonData); err != nil {
 		c.logger.Printf("Error sending ping: %v", err)
 		c.handleReconnection()
 	}
 }
 
 func (c *WSClient) handleReconnection() {
-	c.conn.Close()
+	c.Conn.Close()
 	for i := 0; i < ReconnectionRetries; i++ {
 		if err := c.connect(); err == nil {
 			break
@@ -195,7 +183,7 @@ func (c *WSClient) Authenticate(apiKey, expires, signature string) error {
 
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	return c.conn.WriteMessage(websocket.TextMessage, jsonData)
+	return c.Conn.WriteMessage(websocket.TextMessage, jsonData)
 }
 
 // Close gracefully closes the WebSocket connection
@@ -205,7 +193,7 @@ func (c *WSClient) Close() {
 		defer c.mu.Unlock()
 		c.isClosed = true
 		c.logger.Println("Connection closed")
-		c.conn.Close()
+		c.Conn.Close()
 	})
 }
 

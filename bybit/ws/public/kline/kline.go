400cae9d87a7d477e55ea3638d13a300add95c06
Christos Ploutarchou <cploutarchou@gmail.com>
2023-09-05 19:13:22 +0300

    secure kline package

--- bybit/ws/public/kline/kline.go
+++ bybit/ws/public/kline/kline.go
@@ -3,92 +3,133 @@ package kline
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 
 	"github.com/cploutarchou/crypto-sdk-suite/bybit/ws/client"
 )
 
-type TheKline struct {
-	client      *client.WSClient
-	Messages    chan []byte
-	StopChan    chan struct{}
-	channel     string
-	environment string
-	subChannel  string
+type Interval string
+
+// Kline represents the interface for the kline functionality.
+type Kline interface {
+	SetClient(client *client.WSClient) (Kline, error)
+	Subscribe(symbols []string, interval Interval) (Kline, error)
+	Unsubscribe(topics ...string) (Kline, error)
+	Receive() (int, []byte, error)
+	Close()
+	GetMessagesChan() <-chan []byte
+	Stop()
 }
 
-func (w *TheKline) SetClient(client_ *client.WSClient) *TheKline {
-	w.client = client_
-	return w
+type klineImpl struct {
+	client   *client.WSClient
+	Messages chan []byte
+	StopChan chan struct{}
+	isTest   bool
+	mu       sync.Mutex // Mutex to make operations thread-safe
 }
 
-func (w *TheKline) Subscribe(topics ...string) error {
+func (w *klineImpl) Subscribe(symbols []string, interval Interval) (Kline, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	topics := make([]string, len(symbols))
+	for i, symbol := range symbols {
+		topics[i] = fmt.Sprintf("kline.%s.%s", interval, symbol)
+	}
+
 	topicJSON, err := json.Marshal(topics)
 	if err != nil {
-		return fmt.Errorf("failed to marshal topics: %v", err)
+		return nil, fmt.Errorf("failed to marshal topics: %w", err)
 	}
 
 	message := fmt.Sprintf(`{"op":"subscribe","args":%s}`, string(topicJSON))
-	return w.client.Conn.WriteMessage(client.WSMessageText, []byte(message))
+	err = w.client.Conn.WriteMessage(client.WSMessageText, []byte(message))
+	if err != nil {
+		return nil, fmt.Errorf("failed to subscribe to kline channel: %w", err)
+	}
+
+	return w, nil
 }
 
-func (w *TheKline) Unsubscribe(topics ...string) error {
+func (w *klineImpl) Unsubscribe(topics ...string) (Kline, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
 	topicJSON, err := json.Marshal(topics)
 	if err != nil {
-		return fmt.Errorf("failed to marshal topics: %v", err)
+		return nil, fmt.Errorf("failed to marshal topics: %w", err)
 	}
 
 	message := fmt.Sprintf(`{"op":"unsubscribe","args":%s}`, string(topicJSON))
-	return w.client.Conn.WriteMessage(client.WSMessageText, []byte(message))
+	err = w.client.Conn.WriteMessage(client.WSMessageText, []byte(message))
+	if err != nil {
+		return nil, fmt.Errorf("failed to unsubscribe to kline channel: %w", err)
+	}
+
+	return w, nil
 }
 
-func (w *TheKline) Receive() (int, []byte, error) {
+func (w *klineImpl) Receive() (int, []byte, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
 	return w.client.Conn.ReadMessage()
 }
 
-func (w *TheKline) Close() {
+func (w *klineImpl) Close() {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
 	w.client.Close()
 }
 
-func (w *TheKline) AddSymbols(symbols ...string) error {
-	var subscriptions []string
-	for _, symbol := range symbols {
-		subscription := fmt.Sprintf("kline.%s", symbol)
-		subscriptions = append(subscriptions, subscription)
-	}
-
-	if err := w.Subscribe(subscriptions...); err != nil {
-		return fmt.Errorf("failed to subscribe to kline channel: %v", err)
-	}
-
-	for {
-		select {
-		case <-w.StopChan:
-			return nil
-		default:
-			_, message, err := w.Receive()
-			if err != nil {
-				return fmt.Errorf("error receiving message: %v", err)
-			}
-			w.Messages <- message
-		}
-	}
-}
-
-func (w *TheKline) GetMessagesChan() <-chan []byte {
+func (w *klineImpl) GetMessagesChan() <-chan []byte {
 	return w.Messages
 }
 
-func (w *TheKline) Stop() {
+func (w *klineImpl) Stop() {
 	w.StopChan <- struct{}{}
 }
 
-func New(client_ *client.WSClient, channel, environment, subChannel string) *TheKline {
-	return &TheKline{
-		client:      client_,
-		channel:     channel,
-		environment: environment,
-		subChannel:  subChannel,
-		Messages:    make(chan []byte),
-		StopChan:    make(chan struct{}),
+func (w *klineImpl) SetClient(client *client.WSClient) (Kline, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	w.client = client
+	return w, nil
+}
+
+// New creates a new instance of KlineImpl
+func New(client *client.WSClient, isTestNet bool) Kline {
+	client.Path = "linear"
+	var k klineImpl
+	k.client = client
+	k.Messages = make(chan []byte, 100)
+	k.StopChan = make(chan struct{}, 1)
+	k.isTest = isTestNet
+	err := k.client.Connect()
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return nil
 	}
+
+	<-k.client.Connected
+	fmt.Println("Connected to WS")
+	go func() {
+		for {
+			select {
+			case <-k.StopChan:
+				return
+			default:
+				_, msg, err := k.client.Conn.ReadMessage()
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				k.Messages <- msg
+			}
+		}
+	}()
+	return &k
 }

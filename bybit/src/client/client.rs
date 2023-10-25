4e06780ed44caf37767bf2b44a16c1ba0a05589f
Christos Ploutarchou <cploutarchou@gmail.com>
2023-10-25 23:44:09 +0300

    implement do_request , get and post methods

--- bybit/src/client/client.rs
+++ bybit/src/client/client.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
-use reqwest::Client as HttpClient;
+use std::fmt::format;
+use reqwest::{Client as HttpClient, Response};
+use serde_json::Value::String;
+use url::form_urlencoded;
 
 pub enum Method{
     POST,
@@ -30,5 +33,38 @@ impl Client {
             is_test_net
         }
     }
+
+    pub async fn get(&self,path:&str,params:Params)->Result<Response,reqwest::Error>{
+       self.do_request(Method::GET,path,params)
+    }
+    pub async fn post(&self,path:&str,params:Params)->Result<Response,reqwest::Error>{
+        self.do_request(Method::GET,path,params)
+    }
+
+    async fn do_request(&self,method:Method,path :&str,params:Params)->Result<Response,reqwest::Error>{
+        let base_url = if self.is_test_net{
+            "https://api-testnet.bybit.com"
+        }else {
+            "https://api.bybit.com"
+        };
+        let full_url = format!("{}/{}",base_url,path);
+        let mut http_request = match method {
+            Method::GET => {
+                let query = form_urlencoded::Serializer::new(String::new())
+                    .extend_pairs(params.iter())
+                    .finish();
+                self.http_client.get(&format!("{}?{}", full_url, query))
+            },
+            Method::POST => {
+                let body = serde_json::to_string(&params).unwrap();
+                self.http_client.post(&full_url).body(body)
+            },
+        };
+
+        self.set_common_headers(&mut http_request, &params);
+
+        http_request.send().await
+    }
+
 
 }

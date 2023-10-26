7ca9924e4cb839fb8602cad41a6483ebc1fde5d6
Christos Ploutarchou <cploutarchou@gmail.com>
2023-10-26 23:02:31 +0300

    create bybit client

--- bybit/src/client/client.rs
+++ bybit/src/client/client.rs
@@ -1,53 +1,59 @@
+use reqwest::Client as HttpClient;
+use reqwest::Response;
 use std::collections::HashMap;
-use std::fmt::format;
-use reqwest::{Client as HttpClient, Response};
-use serde_json::Value::String;
+use std::time::{SystemTime, UNIX_EPOCH};
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+use hex;
+use serde_json;
 use url::form_urlencoded;
 
-pub enum Method{
+pub enum Method {
     POST,
     GET,
 }
 
-pub type Params = HashMap<String,String>;
+pub type Params = HashMap<String, String>;
 
-pub struct Request{
-    method:Method,
-    path:String,
-    params:Params
+pub struct Request {
+    method: Method,
+    path: String,
+    params: Params,
 }
 
-pub struct Client{
-    key :String,
-    secret_key : String,
-    http_client :HttpClient,
-    is_test_net:bool,
+pub struct RustClient {
+    key: String,
+    secret_key: String,
+    http_client: HttpClient,
+    is_test_net: bool,
 }
 
-impl Client {
-    pub fn new(key:&str,secret_key:&str,is_test_net:bool)->Client{
-        Client{
-            key:key.to_string(),
+impl RustClient {
+    pub fn new(key: &str, secret_key: &str, is_test_net: bool) -> RustClient {
+        RustClient {
+            key: key.to_string(),
             secret_key: secret_key.to_string(),
             http_client: HttpClient::new(),
-            is_test_net
+            is_test_net,
         }
     }
 
-    pub async fn get(&self,path:&str,params:Params)->Result<Response,reqwest::Error>{
-       self.do_request(Method::GET,path,params)
-    }
-    pub async fn post(&self,path:&str,params:Params)->Result<Response,reqwest::Error>{
-        self.do_request(Method::GET,path,params)
+    pub async fn get(&mut self, path: &str, params: Params) -> Result<Response, reqwest::Error> {
+        self.do_request(Method::GET, path, params).await
     }
 
-    async fn do_request(&self,method:Method,path :&str,params:Params)->Result<Response,reqwest::Error>{
-        let base_url = if self.is_test_net{
-            "https://api-testnet.bybit.com"
-        }else {
-            "https://api.bybit.com"
+    pub async fn post(&mut self, path: &str, params: Params) -> Result<Response, reqwest::Error> {
+        self.do_request(Method::POST, path, params).await
+    }
+
+    async fn do_request(&mut self, method: Method, path: &str, params: Params) -> Result<Response, reqwest::Error> {
+        let base_url = if self.is_test_net {
+            "TestnetBaseURL" // replace with the actual TestnetBaseURL
+        } else {
+            "BaseURL" // replace with the actual BaseURL
         };
-        let full_url = format!("{}/{}",base_url,path);
+
+        let full_url = format!("{}/{}", base_url, path);
         let mut http_request = match method {
             Method::GET => {
                 let query = form_urlencoded::Serializer::new(String::new())
@@ -61,10 +67,36 @@ impl Client {
             },
         };
 
-        self.set_common_headers(&mut http_request, &params);
+       http_request = self.set_common_headers(&mut http_request, &params);
 
         http_request.send().await
     }
 
+    fn set_common_headers(&self, request: &mut reqwest::RequestBuilder, params: &Params) ->reqwest::RequestBuilder {
+        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().to_string();
+        let signature = generate_signature(&self.secret_key, &self.key, &params, &timestamp);
 
+      return  request.header("X-BAPI-SIGN-TYPE", "2")
+            .header("X-BAPI-SIGN", signature)
+            .header("X-BAPI-API-KEY", &self.key)
+            .header("X-BAPI-TIMESTAMP", timestamp)
+            .header("X-BAPI-RECV-WINDOW", "50000")
+            .header("Content-Type", "application/json");
+    }
+}
+
+fn generate_signature(secret_key: &str, api_key: &str, params: &Params, timestamp: &str) -> String {
+    let mut base_string = timestamp.to_string() + api_key + "50000";
+
+    let mut keys: Vec<_> = params.keys().collect();
+    keys.sort();
+
+    for key in keys {
+        base_string.push_str(&format!("{}={}", key, params.get(key).unwrap()));
+    }
+
+    let mut mac = Hmac::<Sha256>::new_from_slice(secret_key.as_bytes()).unwrap();
+    mac.update(base_string.as_bytes());
+
+    hex::encode(mac.finalize().into_bytes())
 }

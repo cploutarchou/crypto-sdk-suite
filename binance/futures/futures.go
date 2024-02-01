98b2a005676dab202839a21d7adc6d1d070a55bc
Christos Ploutarchou <cploutarchou@gmail.com>
2024-02-01 20:00:25 +0200

    start implementing futures interface

--- binance/futures/futures.go
+++ binance/futures/futures.go
@@ -1,7 +1,34 @@
-package binance
+package futures
 
-import "github.com/cploutarchou/crypto-sdk-suite/bybit/market"
+import (
+	"github.com/cploutarchou/crypto-sdk-suite/binance/futures/client"
+	"github.com/cploutarchou/crypto-sdk-suite/binance/futures/market"
+)
 
-type Binance interface {
+type Futures interface {
 	Market() market.Market
+	Account() Account
+	Generic() Generic
+}
+
+type futureImpl struct {
+	client *client.Client
+}
+
+func New(apiKey, apiSecret string, isTestnet bool) Futures {
+	return &futureImpl{
+		client: client.NewFuturesClient(apiKey, apiSecret, isTestnet),
+	}
+}
+
+func (f *futureImpl) Account() Account {
+	return NewAccount(f.client)
+}
+
+func (f *futureImpl) Generic() Generic {
+	return NewGeneric(f.client)
+}
+
+func (f *futureImpl) Market() market.Market {
+	return market.NewMarket(f.client)
 }

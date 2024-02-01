b0fe2a68f2ff23b372c4af7519dea632373e2ebe
Christos Ploutarchou <cploutarchou@gmail.com>
2024-02-01 19:44:53 +0200

    convert account package to interface

--- binance/futures/account.go
+++ binance/futures/account.go
@@ -1,43 +1,53 @@
-package binance
+package futures
 
 import (
 	"fmt"
 	"io"
 	"net/http"
 
-	"github.com/cploutarchou/crypto-sdk-suite/binance/futures/requests"
+	"github.com/cploutarchou/crypto-sdk-suite/binance/futures/client"
 )
 
+// Endpoints used in the futures package.
 const (
 	changePositionModeEndpoint = "/fapi/v1/positionSide/dual"
 )
 
-type Account struct {
-	*requests.Client
+// Account defines the interface for account operations.
+type Account interface {
+	ChangePositionMode(enable bool) error
 }
 
-func NewAccount(client *requests.Client) *Account {
-	return &Account{
-		Client: client,
+// accountImpl implements Account interface using Binance futures API.
+type accountImpl struct {
+	client *client.Client
+}
+
+// NewAccount creates a new Account instance.
+func NewAccount(client *client.Client) Account {
+	return &accountImpl{
+		client: client,
 	}
 }
-func (b *Account) ChangePositionMode(enable bool) error {
+
+// ChangePositionMode toggles the position mode for a futures account.
+func (a *accountImpl) ChangePositionMode(enable bool) error {
 	endpoint := changePositionModeEndpoint
 	data := fmt.Sprintf("dualSidePosition=%v", enable)
 
-	resp, err := b.MakeAuthenticatedRequest(http.MethodPost, endpoint, data)
+	resp, err := a.client.MakeAuthenticatedRequest(http.MethodPost, endpoint, data)
 	if err != nil {
-		return err
+		return fmt.Errorf("error making authenticated request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	responseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return fmt.Errorf("error reading response body: %w", err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("request failed with status code: %d, Response: %s", resp.StatusCode, string(responseBody))
+		return fmt.Errorf("request failed with status code: %d, response: %s", resp.StatusCode, responseBody)
 	}
 
 	return nil

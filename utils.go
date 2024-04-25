023da7c978b9b6ece2ec69925d8bd776964af67c
Christos Ploutarchou <cploutarchou@gmail.com>
2024-04-25 17:41:23 +0300

    update documentation

--- utils.go
+++ utils.go
@@ -1,3 +1,4 @@
+// Package main contains utility functions for printing JSON and handling errors.
 package main
 
 import (
@@ -5,6 +6,11 @@ import (
 	"log"
 )
 
+// printJSON prints the JSON representation of the given value with indentation.
+//
+// The function takes an interface{} as input and marshals it to JSON using json.MarshalIndent.
+// If there's an error during marshaling, it logs the error and returns.
+// Otherwise, it logs the JSON string.
 func printJSON(v interface{}) {
 	d, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
@@ -14,12 +20,21 @@ func printJSON(v interface{}) {
 	log.Println(string(d))
 }
 
+// handleError logs the given error using log.Fatal if it's not nil.
+//
+// The function takes an error as input and checks if it's not nil.
+// If the error is not nil, it logs the error using log.Fatal.
 func handleError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// handleErrorWithPrint handles the given error and prints the data if there's no error.
+//
+// The function takes an interface{} and an error as input.
+// If the error is not nil, it calls handleError to log the error.
+// If the error is nil, it calls printJSON to print the data as JSON.
 func handleErrorWithPrint(data interface{}, err error) {
 	if err != nil {
 		handleError(err)

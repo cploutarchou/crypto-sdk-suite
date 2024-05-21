b6e0bc9789d775f7bc84cfb95d42ad4db13caf67
Christos Ploutarchou <cploutarchou@gmail.com>
2024-05-21 23:01:20 +0300

    update uni-tests for ws

--- bybit/ws/client/client_test.go
+++ bybit/ws/client/client_test.go
@@ -1,3 +1,4 @@
+// Package client provides functions to interact with the Bybit API.
 package client
 
 import (
@@ -10,51 +11,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Constants
 const (
 	testnetBaseURL = "wss://stream-testnet.bybit.com/v5"
 )
 
+// Environment variables
 var (
 	testnetAPIKey    = os.Getenv("BYBIT_FUTURES_TESTNET_API_KEY")
 	testnetAPISecret = os.Getenv("BYBIT_FUTURES_TESTNET_API_SECRET")
 )
 
 // TestNewPublicClient verifies the NewPublicClient function initializes a public client correctly.
+// It tests if the client is initialized with the correct testnet flag and channel type.
 func TestNewPublicClient(t *testing.T) {
-	isTestNet := true
-
-	client, err := NewPublicClient(isTestNet)
+	client, err := NewPublicClient(true)
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
-	assert.Equal(t, isTestNet, client.IsTestNet)
+	assert.Equal(t, true, client.IsTestNet)
 	assert.Equal(t, ChannelType(Public), client.Channel)
 }
 
 // TestNewPrivateClient verifies the NewPrivateClient function initializes a private client correctly.
+// It tests if the client is initialized with the correct API key, API secret, testnet flag,
+// channel type, and max active time.
 func TestNewPrivateClient(t *testing.T) {
 	apiKey := testnetAPIKey
 	apiSecret := testnetAPISecret
-	isTestNet := true
+
 	maxActiveTime := "1m"
 
-	client, err := NewPrivateClient(apiKey, apiSecret, isTestNet, maxActiveTime)
+	client, err := NewPrivateClient(apiKey, apiSecret, true, maxActiveTime)
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
 	assert.Equal(t, apiKey, client.ApiKey)
 	assert.Equal(t, apiSecret, client.ApiSecret)
-	assert.Equal(t, isTestNet, client.IsTestNet)
+	assert.Equal(t, true, client.IsTestNet)
 	assert.Equal(t, ChannelType(Private), client.Channel)
 	assert.Equal(t, maxActiveTime, client.MaxActiveTime)
 }
 
 // TestClient_Connect verifies the Connect function establishes a WebSocket connection correctly.
+// It tests if the function connects to the correct WebSocket URL and sets the appropriate callbacks.
 func TestClient_Connect(t *testing.T) {
 	apiKey := testnetAPIKey
 	apiSecret := testnetAPISecret
-	isTestNet := true
 	maxActiveTime := "1m"
 
-	client, err := NewPrivateClient(apiKey, apiSecret, isTestNet, maxActiveTime)
+	client, err := NewPrivateClient(apiKey, apiSecret, true, maxActiveTime)
 	assert.NoError(t, err)
 
 	client.wsURL = testnetBaseURL + "/private"
@@ -73,13 +77,13 @@ func TestClient_Connect(t *testing.T) {
 }
 
 // TestClient_Send verifies the Send function sends a message correctly.
+// It tests if the function sends a message to the WebSocket server.
 func TestClient_Send(t *testing.T) {
 	apiKey := testnetAPIKey
 	apiSecret := testnetAPISecret
-	isTestNet := true
 	maxActiveTime := "1m"
 
-	client, err := NewPrivateClient(apiKey, apiSecret, isTestNet, maxActiveTime)
+	client, err := NewPrivateClient(apiKey, apiSecret, true, maxActiveTime)
 	assert.NoError(t, err)
 
 	client.wsURL = testnetBaseURL + "/private"
@@ -92,14 +96,14 @@ func TestClient_Send(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-// TestClient_Receive verifies the Receive function receives a message correctly.
+// TestClient_Receive verifies the Receiving function receives a message correctly.
+// It tests if the function receives a message from the WebSocket server and contains the expected content.
 func TestClient_Receive(t *testing.T) {
 	apiKey := testnetAPIKey
 	apiSecret := testnetAPISecret
-	isTestNet := true
 	maxActiveTime := "1m"
 
-	client, err := NewPrivateClient(apiKey, apiSecret, isTestNet, maxActiveTime)
+	client, err := NewPrivateClient(apiKey, apiSecret, true, maxActiveTime)
 	assert.NoError(t, err)
 
 	client.wsURL = testnetBaseURL + "/private"
@@ -121,7 +125,11 @@ func TestClient_Receive(t *testing.T) {
 	go func() {
 		time.Sleep(3 * time.Second)
 		requestPayload := `{"op":"ping","req_id":"` + reqID + `"}`
-		client.Conn.WriteMessage(websocket.TextMessage, []byte(requestPayload))
+		err = client.Conn.WriteMessage(websocket.TextMessage, []byte(requestPayload))
+		if err != nil {
+			t.Errorf("Error sending pong message: %v", err)
+			return
+		}
 	}()
 
 	// Now test receiving a pong message
@@ -133,13 +141,13 @@ func TestClient_Receive(t *testing.T) {
 }
 
 // TestClient_Close verifies the Close function closes the connection correctly.
+// It tests if the function closes the WebSocket connection and sets the appropriate flag.
 func TestClient_Close(t *testing.T) {
 	apiKey := testnetAPIKey
 	apiSecret := testnetAPISecret
-	isTestNet := true
 	maxActiveTime := "1m"
 
-	client, err := NewPrivateClient(apiKey, apiSecret, isTestNet, maxActiveTime)
+	client, err := NewPrivateClient(apiKey, apiSecret, true, maxActiveTime)
 	assert.NoError(t, err)
 
 	client.wsURL = testnetBaseURL + "/private"
